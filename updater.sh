7b599b961ebc0b6002bf09a99bc3c5c401b2de97
Piotr Halama <skrzynka@halamix2.pl>
2023-08-25 19:10:01 +0200

    210

---
+++ updater.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/home/halamix2/teletext/updater \
+  -delay 10s \
+  -delayGSPS 10s \
+  -input /home/halamix2/teletext/prezentacja_input \
+  -output /home/halamix2/teletext/prezentacja_wst
